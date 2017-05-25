2ea337f53508c163fc46ae1adb4c6e3fd1e7fc1c
CINOEA <rodin.ekat@gmail.com>
2017-05-25 08:00:21 +0500

    Edited small defects

--- base.js
+++ base.js
@@ -3,181 +3,169 @@ var canvas = document.getElementById("canvas1");
 var canvasHeight = parseInt(canvas.getAttribute("height"));
 var canvasWidth = parseInt(canvas.getAttribute("width"));
 var context = canvas.getContext('2d');
-	var Transform = function (str,rulOf,rulTo,n) {
-		var retStr = "";
-		for(n; n>0; n--)
-		{
-			var j = 0;
-			retStr = "";
-			for(var i = 0; i<str.length; i++)
-			{
-				if(str[i]==rulOf[0])
-				{
-					var eq = true;
-					j = i;
-					for(i;i-j<rulOf.length;i++)
-					{
-						if(str[i]!=rulOf[i-j])
-						{
-							eq = false;
-							break;
-						}
-					}
-					if(eq)
-					{
-						retStr += rulTo;
-						i = j + rulOf.length-1;
-						
-					}
-					else
-					{
-						i = j;
-						retStr += str[i];
+var Transform = function (str, rulOf, rulTo, n) {
+	var retStr = "";
+	for (n; n > 0; n--) {
+		var j = 0;
+		retStr = "";
+		for (var i = 0; i < str.length; i++) {
+			if (str[i] == rulOf[0]) {
+				var eq = true;
+				j = i;
+				for (i; i - j < rulOf.length; i++) {
+					if (str[i] != rulOf[i - j]) {
+						eq = false;
+						break;
 					}
 				}
-				else
+				if (eq) {
+					retStr += rulTo;
+					i = j + rulOf.length - 1;
+
+				}
+				else {
+					i = j;
 					retStr += str[i];
+				}
 			}
-			str = retStr;
+			else
+				retStr += str[i];
 		}
-	return retStr;
+		str = retStr;
 	}
-	var Print = function (str,x,y,s,q)
-	{
-		
-		context.lineWidth = "2";
-		context.strokeStyle = "black";
-		var a = 0;
-		q = q*deg;
-		var mem = [];
+	return retStr;
+}
+var Print = function (str, x, y, s, q) 
+{
+
+	context.lineWidth = "2";
+	context.strokeStyle = "black";
+	var a = 0;
+	q = q * deg;
+	var mem = [];
+	context.save();
+	context.beginPath();
+	for (var i = 0; i < str.length; i++) {
 		context.save();
-		context.beginPath();
-		for(var i = 0; i<str.length; i++)
-		{
-			context.save();
-			switch (str[i])
-			{
-				case 'F':
+		switch (str[i]) {
+			case 'F':
 				context.moveTo(x, y);
-				var xs = x+s;
-				var x1 = (xs - x)*Math.cos(a)+x; // (y-y)*Math.sin(a) = 0 
-				var y1 = (xs - x)*Math.sin(a)+y; // (y-y)*Math.sin(a) = 0 
+				var xs = x + s;
+				var x1 = (xs - x) * Math.cos(a) + x; // (y-y)*Math.sin(a) = 0 
+				var y1 = (xs - x) * Math.sin(a) + y; // (y-y)*Math.sin(a) = 0 
 				x = x1;
 				y = y1;
 				context.lineTo(x, y);
 				context.restore();
 				break;
-				
-				case '+':
+
+			case '+':
 				a -= q;
 				break;
-				
-				case '-':
+
+			case '-':
 				a += q;
 				break;
-				
-				case '[':
+
+			case '[':
 				mem.push(x);
 				mem.push(y);
 				mem.push(a);
 				break;
-				
-				case ']':
+
+			case ']':
 				a = mem.pop();
 				y = mem.pop();
 				x = mem.pop();
 				break;
-				
-				default:
+
+			default:
 				break;
-			}
-		}
-		context.closePath();
-		context.fill(); 
-		context.stroke(); 
-		context.restore();
-	}
-	function Run () {
-	Print(Transform("[F]+[F]+[F]+[F]+[F]+[F]","F","F[+FF][-FF]FF[+F][-F]FF",4),1000,1000,1,60);
-	}
-	
-	function Qcheck(q){
-		var piFound = false;
-		for(var i = 0; i<q.length; i++)
-		{
-			if(q[i] == "p" && i<(q.length-3))
-			{
-				if(q[i+1] == "i")
-				{
-					piFound = true;
-				}
-			}
-		}
-		if(q=="")
-			return "";
-		if(!isNaN(q))
-			return parseFloat(q);
-		if(piFound==false)
-			return "";
-		else
-		{
-			var numerator = "";
-			var denominator = "";
-			var dFound = false;
-			for(var i = 0; i<q.length; i++)
-			{	
-				if(q[i]=="p")
-					i+=2;
-				if(q[i]=="/")
-				{
-						dFound = true;
-						i++;
-				}
-				if(dFound)
-					denominator+=q[i];
-				else
-					numerator+=q[i];
-			}
-			if(numerator=="")
-				numerator="1";
-			if(isNaN(numerator))
-				return 60;
-			if(isNaN(denominator))
-				return 60;
-			return ((parseFloat(numerator)*180)/parseFloat(denominator));
 		}
 	}
-	
-	function lsystem () {
-		context.clearRect(0,0,canvas.width,canvas.height);
-		var axiom = "F";
-		var n = 5;
-		var xO = 100;
-		var yO = 100;
-		var s = 10;
-		var q = 22.5;
-		var ofWhat = "F";
-		var toWhat = "-F+F+[+F-F-]-[-F+F+F]";
-		if(!isNaN(parseInt(document.getElementById("inputN").value)))
-			n = Math.abs(parseInt(document.getElementById("inputN").value));
-		if(!isNaN(parseInt(document.getElementById("inputX").value)))
-			xO = Math.abs(parseInt(document.getElementById("inputX").value));
-		if(!isNaN(parseInt(document.getElementById("inputY").value)))
-			yO = Math.abs(parseInt(document.getElementById("inputY").value));
-		if(!isNaN(parseInt(document.getElementById("inputS").value)))
-			s = Math.abs(parseInt(document.getElementById("inputS").value));
-		q = document.getElementById("inputQ").value;
-		q = Qcheck(q);
-		axiom = document.getElementById("inputAxiom").value;
-		ofWhat = document.getElementById("inputOF").value;
-		toWhat = document.getElementById("inputTO").value;
-		if(q=="")
-			q = 22.5;
-		if(axiom=="")
-			axiom = "F";
-		if(ofWhat=="")
-			ofWhat = "F";
-		if(toWhat=="")
-			toWhat = "-F+F+[+F-F-]-[-F+F+F]";
-		Print(Transform(axiom,ofWhat,toWhat,n),xO,yO,s,q);
+	context.closePath();
+	context.fill();
+	context.stroke();
+	context.restore();
+}
+function Run() 
+{
+	Print(Transform("[F]+[F]+[F]+[F]+[F]+[F]", "F", "F[+FF][-FF]FF[+F][-F]FF", 4), 1000, 1000, 1, 60);
+}
+
+function Qcheck(q) 
+{
+	var piFound = false;
+	for (var i = 0; i < q.length; i++) {
+		if (q[i] == "p" && i < (q.length - 3)) {
+			if (q[i + 1] == "i") {
+				piFound = true;
+			}
+		}
 	}
+	if (q == "")
+		return "";
+	if (!isNaN(q))
+		return parseFloat(q);
+	if (piFound == false)
+		return "";
+	else {
+		var numerator = "";
+		var denominator = "";
+		var dFound = false;
+		for (var i = 0; i < q.length; i++) {
+			if (q[i] == "p")
+				i += 2;
+			if (q[i] == "/") {
+				dFound = true;
+				i++;
+			}
+			if (dFound)
+				denominator += q[i];
+			else
+				numerator += q[i];
+		}
+		if (numerator == "")
+			numerator = "1";
+		if (isNaN(numerator))
+			return 60;
+		if (isNaN(denominator))
+			return 60;
+		return ((parseFloat(numerator) * 180) / parseFloat(denominator));
+	}
+}
+
+function lsystem() 
+{
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	var axiom = "F";
+	var n = 5;
+	var xO = 100;
+	var yO = 100;
+	var s = 10;
+	var q = 22.5;
+	var ofWhat = "F";
+	var toWhat = "-F+F+[+F-F-]-[-F+F+F]";
+	if (!isNaN(parseInt(document.getElementById("inputN").value)))
+		n = Math.abs(parseInt(document.getElementById("inputN").value));
+	if (!isNaN(parseInt(document.getElementById("inputX").value)))
+		xO = Math.abs(parseInt(document.getElementById("inputX").value));
+	if (!isNaN(parseInt(document.getElementById("inputY").value)))
+		yO = Math.abs(parseInt(document.getElementById("inputY").value));
+	if (!isNaN(parseInt(document.getElementById("inputS").value)))
+		s = Math.abs(parseInt(document.getElementById("inputS").value));
+	q = document.getElementById("inputQ").value;
+	q = Qcheck(q);
+	axiom = document.getElementById("inputAxiom").value;
+	ofWhat = document.getElementById("inputOF").value;
+	toWhat = document.getElementById("inputTO").value;
+	if (q == "")
+		q = 22.5;
+	if (axiom == "")
+		axiom = "F";
+	if (ofWhat == "")
+		ofWhat = "F";
+	if (toWhat == "")
+		toWhat = "-F+F+[+F-F-]-[-F+F+F]";
+	Print(Transform(axiom, ofWhat, toWhat, n), xO, yO, s, q);
+}
